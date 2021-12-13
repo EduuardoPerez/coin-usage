514e0af0d1140f30c42cb2fd6e37862b2a7d1d28
Eduardo Perez <edujpp1@gmail.com>
2021-12-13 08:34:17 -0300

    feat: create home page

--- frontend/src/components/Error.jsx
+++ frontend/src/components/Error.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import '@styles/Error.scss';
 
 const Error = ({ message }) => (
     <p className="error">{message}</p>
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,33 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import '@styles/Home.scss';
 
 const Home = () => {
 	return (
-		<>
-			<h1>Home</h1>
-		</>
+		<div className="home">
+			<div className="home-container">
+				<Link to="/send-coin">
+					<button className="primary-button">
+						Send coins
+					</button>
+				</Link>
+				<Link to="/balances">
+					<button className="primary-button">
+						Show balances
+					</button>
+				</Link>
+				<Link to="/account-transactions">
+					<button className="primary-button">
+						Show account transactions
+					</button>
+				</Link>
+				<Link to="/global-transactions">
+					<button className="primary-button">
+						Show global transactions
+					</button>
+				</Link>
+			</div>
+		</div>
 	);
 }
 
